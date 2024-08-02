91c88a9cec30156cc994bfcf15d2c44179c1453e
Michael Scalzetti <contact@scalz.cloud>
2024-08-02 02:47:40 -0400

    Basic main program to test stuff

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
+#include "location.h"
+#include "node.h"
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
 
 int main(){
-    printf("Hello\n");
+    location start = {00,5,0};
+    location end   = {10,0,0};
+    location loca  = {0,10,0};
+    location locb  = {5,5,0};
+
+    node a = {loca,0};
+    node b = {locb,0};
+
+    a = set_node_weight(a, start, end);
+    b = set_node_weight(b, start, end);
+
+    printf("a:%d - b:%d\n", a.weight,b.weight);
+
+
+    //printf("Hello\n");
+
     return 0;
 }
