510b8e0b3a08d89de9b894f86ca56ea749eba8ec
Michael Scalzetti <contact@scalz.cloud>
2024-08-02 02:26:50 -0400

    prevent double decl

--- src/location.h
+++ src/location.h
@@ -1,3 +1,6 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+
 #include <stdbool.h>
 
 typedef struct{
@@ -7,3 +10,5 @@ typedef struct{
 } location;
 
 bool location_are_equal(location a, location b);
+
+#endif
