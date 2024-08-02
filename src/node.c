1481d3e19b8daf6ed7cf2bf0291d9e75101272ff
Michael Scalzetti <contact@scalz.cloud>
2024-08-02 01:53:27 -0400

    wrote up nodes that take advantage of locations

---
+++ src/node.c
@@ -0,0 +1,18 @@
+#include "location.h"
+#include "utils.h"
+
+typedef struct{
+    location loc;
+    float weight;
+} node;
+
+bool compare_nodes(node a, node b){
+    return a.weight - b.weight;
+}
+
+void set_node_weight(node n, location start, location end){
+    int deltaEnd = abs( n.loc.row - end.row ) + abs( n.loc.col - end.col );
+    int deltaStart = abs( n.loc.row - start.row ) + abs( n.loc.col - start.col );
+    n.weight = deltaEnd + deltaStart;
+}
+
